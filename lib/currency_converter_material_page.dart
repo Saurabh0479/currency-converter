4c34977e357476bc56923904fbd0c5ee4cc9a678
Saurabh Kumar Gupta <saurabhguptavirat@gmail.com>
2026-06-26 17:16:25 +0530

    Buttons Added

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CurrencyConverterMaterialPage extends StatelessWidget {
@@ -6,7 +7,7 @@ const CurrencyConverterMaterialPage({super.key});
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      backgroundColor: Colors.teal,
+      backgroundColor: const Color.fromARGB(255, 68, 105, 255),
         body: Center(
           child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -57,7 +58,34 @@ const CurrencyConverterMaterialPage({super.key});
                   ),
                 ),
               ),
-              
+
+              // button
+
+              // raised
+              // appears like a text
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ElevatedButton(
+                  onPressed: (){
+                    // debug, release, profile
+                    if(kDebugMode){
+                    print('Button Clicked');
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(  
+                    // elevation: WidgetStatePropertyAll(10),
+                    backgroundColor: (Colors.black),
+                    foregroundColor: (Colors.white),
+                    // minimumSize: WidgetStatePropertyAll(
+                    //const Size(double.infinity, 50),
+                    // ),  
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
+                      ),
+                    ),
+                  child: const Text('Convert'),
+                  ),
+               ),
             ],
           ),
         ), 

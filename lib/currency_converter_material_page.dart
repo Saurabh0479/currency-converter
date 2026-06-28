74bb4705460b6f684a8496b0c40c81495b6e1b0f
Saurabh Kumar Gupta <saurabhguptavirat@gmail.com>
2026-06-28 20:28:05 +0530

    AppBar

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CurrencyConverterMaterialPage extends StatelessWidget {
@@ -6,16 +6,29 @@ const CurrencyConverterMaterialPage({super.key});
 
   @override
   Widget build(BuildContext context){
+    double result = 0;
+    final TextEditingController textEditingController = TextEditingController();                                                 
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 68, 105, 255),
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 68, 105, 255),
+          elevation: 0,
+          title: const Text(
+            'CURRENCY CONVERTER',
+            style: TextStyle(
+              color: Colors.white,
+            )
+          ),
+          centerTitle: true,
+        ),
         body: Center(
           child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           // crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            const Text(
-              '0',
-              style: TextStyle(
+             Text(
+              result.toString() ,
+              style: const TextStyle(
                 fontSize: 60,
                 fontWeight: FontWeight.bold,
                 color: Color.fromARGB(255, 255, 255, 255),
@@ -24,6 +37,7 @@ const CurrencyConverterMaterialPage({super.key});
               Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: TextField(
+                  controller: textEditingController,
                   style: const TextStyle(
                     color: Colors.black,
                   ),
@@ -67,18 +81,13 @@ const CurrencyConverterMaterialPage({super.key});
                 padding: const EdgeInsets.all(10.0),
                 child: ElevatedButton(
                   onPressed: (){
-                    // debug, release, profile
-                    if(kDebugMode){
-                    print('Button Clicked');
-                    }
+                    result = double.parse(textEditingController.text)*94.36;
                   },
                   style: ElevatedButton.styleFrom(  
                     // elevation: WidgetStatePropertyAll(10),
-                    backgroundColor: (Colors.black),
-                    foregroundColor: (Colors.white),
-                    // minimumSize: WidgetStatePropertyAll(
-                    //const Size(double.infinity, 50),
-                    // ),  
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.white,
+                    // minimumSize: const Size(double.infinity, 50),
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.all(Radius.circular(5)),
                       ),
